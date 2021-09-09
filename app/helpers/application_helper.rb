ffa5b5d0c38d6999e469f9c103b3160185b3d3b8
Vladislav Kuznecov <fizvlad@mail.ru>
2021-09-10 01:25:22 +0300

    feat: title tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def title_tag(title, env_prefix: true)
+    return tag.title(title) unless env_prefix
+
+    prefix =
+      case Rails.env
+      when 'development' then 'DEV'
+      else ''
+      end
+    return tag.title(title) if prefix.blank?
+
+    tag.title("#{prefix} | #{title}")
+  end
 end
